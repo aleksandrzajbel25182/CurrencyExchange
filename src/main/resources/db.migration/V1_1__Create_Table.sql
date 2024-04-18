7cfd531e81d4869ccabe8165258b813105e84bbd
Александр Зайбель <aleksandrzajbel25182@gmail.com>
2024-04-18 14:38:34 +0700

    Create sql scripts database

---
+++ src/main/resources/db.migration/V1_1__Create_Table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE currencies
+(
+    id       SERIAL PRIMARY KEY,
+    code     VARCHAR(10)  NOT NULL,
+    fullname VARCHAR(100) NOT NULL,
+    sign     VARCHAR(10)  NOT NULL
+);
+
+CREATE TABLE exchangeRates
+(
+    id SERIAL PRIMARY KEY,
+    baseCurrencyId   INT NOT NULL REFERENCES currencies (ID),
+    targetCurrencyId INT NOT NULL REFERENCES currencies (ID),
+    rate DECIMAL NOT NULL
+);
