f9019822e80c10a75d283e59ca127fc17252bf86
Александр Зайбель <aleksandrzajbel25182@gmail.com>
2024-04-23 14:14:03 +0700

    Pretty Printing the Formatted JSON

--- src/main/java/com/currencyexchage/utils/JsonConvert.java
+++ src/main/java/com/currencyexchage/utils/JsonConvert.java
@@ -1,11 +1,12 @@
 package com.currencyexchage.utils;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class JsonConvert {
 
   public static <T> String jsonConvert(T enity) {
-    Gson gson = new Gson();
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
     String json = gson.toJson(enity);
     return json;
   }
