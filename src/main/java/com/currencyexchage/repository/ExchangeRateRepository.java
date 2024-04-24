d13c24418a7ccffa383815304786d68605e1243e
Александр Зайбель <aleksandrzajbel25182@gmail.com>
2024-04-24 16:53:03 +0700

    Implement a specific exchange rate servlet

--- src/main/java/com/currencyexchage/repository/ExchangeRateRepository.java
+++ src/main/java/com/currencyexchage/repository/ExchangeRateRepository.java
@@ -17,6 +17,8 @@ public class ExchangeRateRepository implements CrudRepository<ExchangeRate> {
 
   private final String GET_ALL_EXCHANGE_RATE = "SELECT id,basecurrencyid, targetcurrencyid,rate FROM exchangerates";
 
+  private static final String GET_FIND_BY_СODE = "SELECT id, basecurrencyid, targetcurrencyid, rate FROM exchangerates WHERE basecurrencyid = ? and targetcurrencyid =?";
+
   public ExchangeRateRepository(DataSource dataSource) {
     this.dataSource = dataSource;
     currenciesRepository = new CurrenciesRepository(dataSource);
@@ -55,6 +57,36 @@ public class ExchangeRateRepository implements CrudRepository<ExchangeRate> {
   public ExchangeRate findById(int id) {
     return null;
   }
+
+  public ExchangeRate finByCode(String baseCurrency, String targetCurrency) {
+    ExchangeRate exchangeRate = null;
+    try (Connection connection = dataSource.getConnection();
+        PreparedStatement statement = connection.prepareStatement(GET_FIND_BY_СODE)) {
+
+      var base = currenciesRepository.findByCode(baseCurrency);
+      var target = currenciesRepository.findByCode(targetCurrency);
+
+      statement.setInt(1, base.getId());
+      statement.setInt(2, target.getId());
+
+      ResultSet resultSet = statement.executeQuery();
+
+      if (resultSet.next()) {
+        exchangeRate = new ExchangeRate(
+            resultSet.getInt("id"),
+            currenciesRepository.findById(resultSet.getInt("basecurrencyid")),
+            currenciesRepository.findById(resultSet.getInt("targetcurrencyid")),
+            BigDecimal.valueOf(resultSet.getDouble("rate"))
+        );
+
+      }
+    } catch (
+        SQLException e) {
+      throw new RuntimeException(e);
+    }
+    return exchangeRate;
+  }
+
 
   @Override
   public void update(ExchangeRate entity) {
--- src/main/java/com/servlets/ExchangeRateServlet.java
+++ src/main/java/com/servlets/ExchangeRateServlet.java
@@ -11,23 +11,27 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/exchangeRates")
+@WebServlet("/exchangeRates/*")
 public class ExchangeRateServlet extends HttpServlet {
 
   private ExchangeRateRepository exchangeRateRepository;
-
-  @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
-
-    PrintWriter writer = resp.getWriter();
-    var message = JsonConvert.jsonConvert(exchangeRateRepository.get());
-    writer.write(message);
-  }
 
   @Override
   public void init(ServletConfig config) throws ServletException {
     exchangeRateRepository = (ExchangeRateRepository) config.getServletContext()
         .getAttribute("exchangeRateRepository");
   }
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    String currenciesCodes = req.getPathInfo().replaceFirst("/", "").toUpperCase();
+
+    var base =  currenciesCodes.substring(0, 3);
+    var target = currenciesCodes.substring(3, 6);
+    PrintWriter writer = resp.getWriter();
+    var message = JsonConvert.jsonConvert(exchangeRateRepository.finByCode(base,target));
+    writer.write(message);
+  }
+
+
 }
