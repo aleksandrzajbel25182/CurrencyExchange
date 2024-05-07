ece4893fd408090892f67ec3683d8a6aa987026c
Александр Зайбель <aleksandrzajbel25182@gmail.com>
2024-05-07 11:14:00 +0700

    Create Currencies

--- src/main/java/com/currencyexchage/repository/CurrenciesRepository.java
+++ src/main/java/com/currencyexchage/repository/CurrenciesRepository.java
@@ -1,10 +1,12 @@
 package com.currencyexchage.repository;
 
 import com.currencyexchage.model.Currency;
+import com.currencyexchage.model.CurrencyCB;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import javax.sql.DataSource;
@@ -17,6 +19,7 @@ public class CurrenciesRepository implements CrudRepository<Currency> {
   private static final String GET_ALL_CURRENCIES = "SELECT id, code, fullName, sign FROM currencies ";
   private static final String GET_FIND_BY_ID = "SELECT id, code, fullName, sign FROM currencies WHERE id = ?";
   private static final String GET_FIND_BY_CODE = "SELECT id, code, fullName, sign FROM currencies WHERE code = ?";
+  private static final String INSERT_CURRENCIES = "INSERT INTO currencies (code,fullname,sign) VALUES(?,?,?) ";
 
   public CurrenciesRepository(DataSource dataSource) {
     this.dataSource = dataSource;
@@ -61,6 +64,24 @@ public class CurrenciesRepository implements CrudRepository<Currency> {
     return currency;
   }
 
+  public boolean findCode(String hasCode) {
+    try (Connection connection = dataSource.getConnection();
+        PreparedStatement statement = connection.prepareStatement(GET_FIND_BY_CODE)) {
+
+      statement.setString(1, hasCode);
+      ResultSet resultSet = statement.executeQuery();
+      if (resultSet.next()) {
+        return true;
+      } else {
+        return false;
+      }
+
+
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
   public Currency findByCode(String hasCode) {
     Currency currency = new Currency();
     try (Connection connection = dataSource.getConnection();
@@ -77,6 +98,32 @@ public class CurrenciesRepository implements CrudRepository<Currency> {
       throw new RuntimeException(e);
     }
     return currency;
+  }
+
+
+  public void create(List<CurrencyCB> entity) {
+
+    try (Connection connection = dataSource.getConnection();
+        PreparedStatement statement = connection.prepareStatement(INSERT_CURRENCIES)) {
+
+      for (var currency : entity) {
+
+        if (findCode(currency.getCharCode()) == false) {
+
+          statement.setString(1, currency.getCharCode());
+          statement.setString(2, currency.getName());
+          statement.setString(3, currency.getNumCode());
+          statement.addBatch();
+        }
+
+
+      }
+      statement.executeBatch();
+
+    } catch (SQLException e) {
+     e.printStackTrace();
+    }
+
   }
 
   @Override
