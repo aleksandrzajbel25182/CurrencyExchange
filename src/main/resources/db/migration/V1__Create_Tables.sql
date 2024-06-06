c77b8e9407a92df5466464cb5455a454678cf7d2
Александр Зайбель <aleksandrzajbel25182@gmail.com>
2024-06-06 16:24:40 +0700

    Connect the flyway plugin
    Create a version 1 migration file

--- src/main/resources/db/migration/V1__Create_Tables.sql
+++ src/main/resources/db/migration/V1__Create_Tables.sql
@@ -1,9 +1,8 @@
 CREATE TABLE currencies
 (
     id       SERIAL PRIMARY KEY,
-    code     VARCHAR(10)  NOT NULL,
-    fullname VARCHAR(100) NOT NULL,
-    sign     VARCHAR(10)  NOT NULL
+    charCode     VARCHAR(10)  NOT NULL,
+    fullname VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE exchangeRates
@@ -11,10 +10,6 @@ CREATE TABLE exchangeRates
     id SERIAL PRIMARY KEY,
     baseCurrencyId   INT NOT NULL REFERENCES currencies (ID),
     targetCurrencyId INT NOT NULL REFERENCES currencies (ID),
-    rate DECIMAL NOT NULL
-);
-
-CREATE TABLE data_central_bank
-(
-    currentDate date primary key
+    rate DECIMAL NOT NULL,
+    date DATE NOT NULL
 );
