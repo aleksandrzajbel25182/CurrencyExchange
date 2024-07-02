f8ff0758beafe6674ef58ff4868dd1e9dc29014c
Александр Зайбель <aleksandrzajbel25182@gmail.com>
2024-07-02 12:06:14 +0700

    Clean the Main class

--- UpdateSource/src/main/java/update/Main.java
+++ UpdateSource/src/main/java/update/Main.java
@@ -1,17 +1,9 @@
 package update;
 
-import java.util.HashMap;
 
 public class Main {
 
   public static void main(String[] args) {
-
-    HashMap<String, String> map = new HashMap<String, String>();
-    map.put("x", "y");
-
-    String value = map.get("y"); // value = "y"
-    System.out.println(value);
-
 
   }
 
