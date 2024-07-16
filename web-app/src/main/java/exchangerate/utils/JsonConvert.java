2b6736806e56d10e7dddcc5a87ac1283471e1098
Александр Зайбель <aleksandrzajbel25182@gmail.com>
2024-07-16 11:29:03 +0700

    Serialize LocalDate for json

--- web-app/src/main/java/exchangerate/utils/JsonConvert.java
+++ web-app/src/main/java/exchangerate/utils/JsonConvert.java
@@ -2,11 +2,13 @@ package exchangerate.utils;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import java.time.LocalDate;
 
 public class JsonConvert {
 
   public static <T> String jsonConvert(T enity) {
-    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    Gson gson = new GsonBuilder().registerTypeAdapter(LocalDate.class, new LocalDateTypeAdapter())
+        .setPrettyPrinting().create();
     String json = gson.toJson(enity);
     return json;
   }
