9b4ce00969a8c92f754d06c1cac9ffc8271099be
Александр Зайбель <aleksandrzajbel25182@gmail.com>
2024-08-08 15:29:46 +0700

    Fix migration files

--- web-app/src/main/resources/db/migration/V1__Create_Tables.sql
+++ web-app/src/main/resources/db/migration/V1__Create_Tables.sql
@@ -13,3 +13,14 @@ CREATE TABLE exchangeRates
     rate DECIMAL NOT NULL,
     date DATE NOT NULL
 );
+CREATE TABLE subscriptions
+(
+    id SERIAL PRIMARY KEY,
+    url VARCHAR(255)  NOT NULL,
+    baseCurrencyId INT NOT NULL REFERENCES currencies (ID),
+    targetCurrencyId INT NOT NULL REFERENCES currencies (ID),
+    rate DECIMAL NOT NULL,
+    date DATE NOT NULL ,
+    status VARCHAR(15)
+);
+
