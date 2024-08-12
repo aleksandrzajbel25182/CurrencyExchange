6e4adb7dc456a5464a493560c7394006fec82862
Александр Зайбель <aleksandrzajbel25182@gmail.com>
2024-08-12 14:35:56 +0700

    Handling errors in currencies

--- web-app/src/main/java/exchangerate/servlets/CurrencyServlet.java
+++ web-app/src/main/java/exchangerate/servlets/CurrencyServlet.java
@@ -1,5 +1,6 @@
 package exchangerate.servlets;
 
+import com.entities.Currency;
 import com.repository.CurrenciesRepository;
 import com.util.JsonConvert;
 import jakarta.servlet.ServletConfig;
@@ -10,24 +11,34 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Optional;
 
 @WebServlet("/currency")
 public class CurrencyServlet extends HttpServlet {
 
   private CurrenciesRepository currencyRepository;
+
   @Override
   public void init(ServletConfig config) {
     currencyRepository = (CurrenciesRepository) config.getServletContext()
         .getAttribute("currenciesRepository");
   }
+
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
     PrintWriter writer = resp.getWriter();
 
-    int id = Integer.parseInt(req.getParameter("id"));
-    var message = JsonConvert.jsonConvert(currencyRepository.findById(id));
-    writer.write(message);
+    String code = req.getParameter("code").toUpperCase();
+    Optional<Currency> currency = currencyRepository.findByCode(code);
+
+    if (currency.isPresent()) {
+      String message = JsonConvert.jsonConvert(currency.get());
+      writer.write(message);
+    } else {
+      resp.sendError(HttpServletResponse.SC_NOT_FOUND, "There is no such currency in the database");
+    }
+
   }
 
 
